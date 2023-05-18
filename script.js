7e7542a1ce27fef3d95ad1d7f78dee0da1026bc9
LarcARC <murilo_d3@hotmail.com>
2023-05-18 03:03:01 -0300

    shadow in navbar

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-windows.scroll({
-  top: 0,
-  behavior: "smooth",
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("shadow");
+  } else {
+    navbar.classList.remove("shadow");
+  }
 });
